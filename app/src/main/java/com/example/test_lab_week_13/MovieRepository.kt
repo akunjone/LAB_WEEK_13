6d061bd9bfe184ae52509c84047eeaa2fe83e583
akunjone <103233070+akunjone@users.noreply.github.com>
2025-12-05 15:52:55 +0700

    Commit No. 3 – update to WorkManager

--- app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
@@ -1,5 +1,6 @@
 package com.example.test_lab_week_13
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_13.api.MovieService
@@ -71,5 +72,19 @@ class MovieRepository(private val movieService: MovieService,
         }.flowOn(Dispatchers.IO)
     }
 
-
+    // fetch movies from the API and save them to the database
+// this function is used at an interval to refresh the list of popular movies
+    suspend fun fetchMoviesFromNetwork() {
+        val movieDao: MovieDao = movieDatabase.movieDao()
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            val moviesFetched = popularMovies.results
+            movieDao.addMovies(moviesFetched)
+        } catch (exception: Exception) {
+            Log.d(
+                "MovieRepository",
+                "An error occurred: ${exception.message}"
+            )
+        }
+    }
 }
