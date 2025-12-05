f8b1d1cfb61124b7ce1b1635da881c0118a3c0d8
akunjone <103233070+akunjone@users.noreply.github.com>
2025-12-05 14:56:31 +0700

    Commit No. 2 – update to Room

--- app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
@@ -3,13 +3,17 @@ package com.example.test_lab_week_13
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_13.api.MovieService
+import com.example.test_lab_week_13.database.MovieDao
+import com.example.test_lab_week_13.database.MovieDatabase
 import com.example.test_lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
+class MovieRepository(private val movieService: MovieService,
+                      private val movieDatabase: MovieDatabase
+) {
     private val apiKey = "c279e8f69b7ae09196ddebbbeda48c36"
     // LiveData that contains a list of movies
 //    private val movieLiveData = MutableLiveData<List<Movie>>()
@@ -37,12 +41,35 @@ class MovieRepository(private val movieService: MovieService) {
 // this function returns a Flow of Movie objects
 // a Flow is a type of coroutine that can emit multiple values
 // for more info, see: https://kotlinlang.org/docs/flow.html#flows
+//    fun fetchMovies(): Flow<List<Movie>> {
+//        return flow {
+//// emit the list of popular movies from the API
+//            emit(movieService.getPopularMovies(apiKey).results)
+//// use Dispatchers.IO to run this coroutine on a shared pool of threads
+//        }.flowOn(Dispatchers.IO)
+//    }
+
+    ///function fetchMovies dengan Room database:
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-// emit the list of popular movies from the API
-            emit(movieService.getPopularMovies(apiKey).results)
-// use Dispatchers.IO to run this coroutine on a shared pool of threads
+            // Check if there are movies saved in the database
+            val movieDao: MovieDao = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+            // If there are no movies saved in the database,
+            // fetch the list of popular movies from the API
+            if(savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+                // save the list of popular movies to the database
+                movieDao.addMovies(movies)
+                // emit the list of popular movies from the API
+                emit(movies)
+            } else {
+                // If there are movies saved in the database,
+                // emit the list of saved movies from the database
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
     }
+
 
 }
